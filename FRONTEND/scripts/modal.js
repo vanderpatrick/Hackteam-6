db7a92ecef9ae0898a319b7c91514984e70bdb1c
Sean <seanpatmeade@gmail.com>
2023-06-17 02:34:50 +0100

    Trying to get image into background

--- FRONTEND/scripts/modal.js
+++ FRONTEND/scripts/modal.js
@@ -1,16 +1,27 @@
 const popupModal = (eventDataFromApi) => {
-  Swal.fire({
-    title: eventDataFromApi.event,
-    html:
-      '<p>' + eventDataFromApi.date + '</p>' + '<br/>' +
-      '<p>' + eventDataFromApi.country + ',' + eventDataFromApi.region + '</p>' + '<br/>' +
-      eventDataFromApi.description,
-    showCloseButton: true,
-    focusConfirm: false,
-    confirmButtonText:
-      'Exit',
-    confirmButtonAriaLabel: 'Exit',
-  })
+  console.log(eventDataFromApi.image_link)
+    Swal.fire({
+        title: eventDataFromApi.event,
+        html:
+            "<div >" +
+            "<p>" +
+            eventDataFromApi.date +
+            "</p>" +
+            "<br/>" +
+            "<p>" +
+            eventDataFromApi.country +
+            ", " +
+            eventDataFromApi.region +
+            "</p>" +
+            "<br/>" +
+            eventDataFromApi.description +
+            "</div>",
+        showCloseButton: true,
+        focusConfirm: false,
+        confirmButtonText: "Exit",
+        confirmButtonAriaLabel: "Exit",
+    });
+    document.getElementsByClassName('swal2-popup').setAttribute('style', 'background-image: url('+ eventDataFromApi.image_link +') !important');
 };
 
 export default popupModal;
