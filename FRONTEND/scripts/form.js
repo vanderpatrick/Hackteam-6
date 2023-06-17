8561990cdce339f20f662bc945b898d2a1519541
Sean <seanpatmeade@gmail.com>
2023-06-17 22:55:23 +0100

    Add modal to contact and check if fields are blank

--- FRONTEND/scripts/form.js
+++ FRONTEND/scripts/form.js
@@ -1,13 +1,15 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (event) {
-  console.log(event);
   event.preventDefault();
 
   const formData = new FormData(form);
 
   const jsonData = JSON.stringify(Object.fromEntries(formData));
-  console.log(jsonData);
+  if (Object.fromEntries(formData).email == "" || Object.fromEntries(formData).country == "" || Object.fromEntries(formData).description == "") {
+    Swal.fire('Error', 'Fields cannot be blank.', 'error');
+    return;
+  } else {
 
   fetch('https://pride-api.onrender.com/api/contacts', {
     method: 'POST',
@@ -25,4 +27,5 @@ form.addEventListener('submit', function (event) {
       console.error(error);
       Swal.fire('Error', 'An error occurred while submitting the form.', 'error');
     });
+  }
 });
