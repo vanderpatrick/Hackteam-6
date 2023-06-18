53300ea52505dcd68491e573210c6deaa7644f69
pjrclarke <>
2023-06-18 12:58:51 +0000

    form.reset, error on world map and sizing issues on contact page

--- FRONTEND/scripts/country-search.js
+++ FRONTEND/scripts/country-search.js
@@ -4,14 +4,12 @@ if (document.getElementById("searchButton") != null) {
     document.getElementById("searchButton").onclick = function () {
         var searchInput = document.getElementById("searchInput");
         var inputText = searchInput.value;
-        console.log(inputText);
 
         requestCountry(inputText);
     };
 }
 
 function requestCountry(countryName) {
-    console.log("sending request");
     $.ajax({
         type: "GET",
         url: "https://pride-api.onrender.com/api/events/" + countryName,
@@ -19,17 +17,27 @@ function requestCountry(countryName) {
             // get country for event from api
             let apiCountryName = eventsDataFromApi.country;
             // If the countries match up
-            if (apiCountryName == countryName) {
-                popupModal(eventsDataFromApi);
-            } else {
-                // If no data is in the database for the clicked country display generic message
-                Swal.fire({
-                    title: countryName,
-                    text: "Sorry we currently have no information on this country. Please head to the Submit page and tell us of any events you know have happened in this are. We would love to add them.",
-                    confirmButtonText: "Close",
-                });
-            }
+            popupModal(eventsDataFromApi);
         },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status === 500) {
+              // Handle 500 Internal Server Error
+              Swal.fire({
+                title: countryName,
+                text: "Sorry we currently have no information on this country. Please head to the Submit page and tell us of any events you know have happened in this are. We would love to add them.",
+                confirmButtonText: "Close",
+            });
+            } else {
+              // Handle other error cases
+              console.log("AJAX request error:", textStatus, errorThrown);
+              Swal.fire({
+                title: "Error",
+                text: "An error occurred while fetching data for " + countryName + ". Please try again later.",
+                confirmButtonText: "Close",
+                icon: "error"
+              });
+            }
+          }
     });
 }
 
--- FRONTEND/scripts/form.js
+++ FRONTEND/scripts/form.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', function (event) {
 
   const jsonData = JSON.stringify(Object.fromEntries(formData));
   if (Object.fromEntries(formData).email == "" || Object.fromEntries(formData).country == "" || Object.fromEntries(formData).description == "") {
-    Swal.fire('Error', 'Fields cannot be blank.', 'error');
+    Swal.fire('Error', "Don't leave any fields blank, please! We want as much information as possible.", 'error');
     return;
   } else {
 
@@ -21,11 +21,12 @@ form.addEventListener('submit', function (event) {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      Swal.fire('Success', 'Form submitted successfully!', 'success');
+      Swal.fire('Success', "Thanks for sending in your response! We'll check the information you've provided and if it checks out, you'll see it uploaded onto our map soon!", 'success');
     })
     .catch(error => {
       console.error(error);
       Swal.fire('Error', 'An error occurred while submitting the form.', 'error');
     });
   }
+  form.reset();
 });
